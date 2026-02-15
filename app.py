7b12c52e2fa4bd3fc063bf28a6be09a1ea7c6bbc
khongkoemtong <kkoemtong@gmail.com>
2026-02-15 11:56:45 +0700

    middleware

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from functools import wraps
 
 app = Flask(__name__)
 
+app.secret_key = "123"
 
 def connect_db():
     conection = pymysql.connect(
@@ -24,11 +25,23 @@ def connect_db():
     return conection
 connect_db()
 
+
+def login_require(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        
+        if 'user_id' not in session:
+            
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+        
+    return decorated_function
+
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER']=UPLOAD_FOLDER
 
 @app.route('/')
-
+@login_require
 def home():
     conect = connect_db()
     cursor = conect.cursor()
@@ -39,8 +52,8 @@ def home():
     return render_template('index.html',myuser =user)
 
 @app.route('/insert',methods=["POST"])
-
-def insert ():
+@login_require
+def insert():
     conect = connect_db()
     cursor = conect.cursor()
 
@@ -61,10 +74,8 @@ def insert ():
     conect.commit()
     return redirect(url_for('home'))
 
-
-
 @app.route('/update',methods=["POST"])
-
+@login_require
 def update():
     conetion = connect_db()
     cursor = conetion.cursor()
@@ -82,7 +93,7 @@ def update():
     return redirect(url_for('home'))
 
 @app.route('/delete',methods=["POST"])
-
+@login_require
 def delete ():
     connection = connect_db()
     cursor = connection.cursor()
@@ -96,9 +107,70 @@ def delete ():
 
 
 
+@app.route('/userpage')
+def userpage ():
+    conection = connect_db()
+    cursor = conection.cursor()
+
+    cursor.execute( "SELECT * FROM user")
+    alluser = cursor.fetchall()
+    return render_template("userpage.html",user=alluser)
 
 
+@app.route('/register',methods=['POST','GET'])
+def register():
+    error=""
+    if request.method=="POST" :
+        contion = connect_db()
+        cursor = contion.cursor()
 
+        username = request.form['username']
+        email = request.form['email']
+        pasword = request.form['password']
+
+       
+        try:
+            sql = "INSERT INTO accounts (username,email,password) values(%s,%s,%s)"
+            cursor.execute(sql,(username,email,pasword))
+            contion.commit()
+            error = "insert success"
+        except Exception as e :
+            error = ("can not insert data",e)
+            
+ 
+
+
+    return render_template("register.html",error =error)
+
+
+@app.route('/login',methods=['POST','GET'])
+def login():
+    error = ""
+    if request.method=='POST':
+
+        contion= connect_db()
+        cursor = contion.cursor()
+        email = request.form['email']
+        password = request.form['password']
+
+        try:
+            sql =("SELECT * FROM  accounts WHERE  email=%s AND password =%s")
+            cursor.execute(sql,(email,password))
+            acount =cursor.fetchone()
+            error="Login success"
+
+            if acount:
+                acount['user_id']=acount[0]
+                acount['user_name']=acount[1]
+                return redirect(url_for('home'))
+            
+        except Exception as e :
+            error="Can not find email and password ",e
+            return redirect(url_for('login'))
+    return render_template('login.html',error=error)
+
+
+        
 
 
 
